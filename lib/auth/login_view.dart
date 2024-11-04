049c138082c27cd38c81bd9373afb198699e6882
Basit ali <basitalyshah51214@gmail.com>
2024-11-04 09:20:31 -0800

    creating login ui

--- lib/auth/login_view.dart
+++ lib/auth/login_view.dart
@@ -8,8 +8,74 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  "Logo",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Email*",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Password*",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black,
       ),
-      home: LoginView(),
+      home: const LoginView(),
     );
   }
 }
