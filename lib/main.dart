905ece918e34a42a79b5c77e6691a215911fbc8d
Basit ali <basitalyshah51214@gmail.com>
2024-11-07 07:12:08 -0800

    init firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tiktok_clone/auth/auth_controller.dart';
 import 'package:tiktok_clone/auth/login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController);
+  });
   runApp(const MyApp());
 }
 
